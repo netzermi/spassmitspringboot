80aa7151a19075841b22000460bf00b354fe6857
Michael Netzer <netzermi@gmail.com>
2025-03-04 10:19:16 +0100

    first commit

---
+++ src/main/java/at/itkollegimst/studentenverwaltung/repositories/DbZugriffStudentenJPA.java
@@ -0,0 +1,51 @@
+package at.itkollegimst.studentenverwaltung.repositories;
+
+
+import at.itkollegimst.studentenverwaltung.domain.Student;
+import at.itkollegimst.studentenverwaltung.exceptions.StudentNichtGefunden;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class DbZugriffStudentenJPA implements DbZugriffStudenten {
+
+    private StudentJPARepo studentJPARepo;
+
+    public DbZugriffStudentenJPA(StudentJPARepo studentJPARepo) {
+        this.studentJPARepo = studentJPARepo;
+    }
+
+    @Override
+    public Student studentSpeichern(Student student) {
+        return this.studentJPARepo.save(student);
+    }
+
+    @Override
+    public List<Student> alleStudenten() {
+        return this.studentJPARepo.findAll();
+    }
+
+    @Override
+    public List<Student> alleStudentenAusDemOrt(String plz) {
+        return this.studentJPARepo.findAllByPlz(plz);
+    }
+
+    @Override
+    public Student studentMitId(Long id) throws StudentNichtGefunden {
+        Optional<Student> optStudent = this.studentJPARepo.findById(id);
+        if(optStudent.isPresent())
+        {
+            return optStudent.get();
+        } else
+        {
+            throw new StudentNichtGefunden("Student mit der Id " + id + " nicht gefunden!");
+        }
+    }
+
+    @Override
+    public void studentLoeschenMitId(Long id) {
+        this.studentJPARepo.deleteById(id);
+    }
+}
---
+++ src/main/java/at/itkollegimst/studentenverwaltung/services/StudentenService.java
@@ -0,0 +1,14 @@
+package at.itkollegimst.studentenverwaltung.services;
+
+import at.itkollegimst.studentenverwaltung.domain.Student;
+import at.itkollegimst.studentenverwaltung.exceptions.StudentNichtGefunden;
+
+import java.util.List;
+
+public interface StudentenService {
+    List<Student> alleStudenten();
+    Student studentEinfuegen(Student student);
+    Student studentMitId(Long id) throws StudentNichtGefunden;
+    List<Student> alleStudentenMitPlz(String plz);
+    void studentLoeschenMitId(Long id);
+}
